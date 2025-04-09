a3ef967f2ec93970b8c2eb3db2e4c73d4608663d
diewland <diewland@gmail.com>
2025-04-09 22:54:47 +0700

    add sample page and publish script

---
+++ publish.sh
@@ -0,0 +1,3 @@
+# https://setting.wal.app
+#site-builder publish --site-name 'Setting Page' --epochs max ./setting
+site-builder update --epochs max ./setting 0xbbdbcf6b38f76e8dc2bf75b5b17fe8c25a5147fec1f6fe9943f0830af50f9e51
