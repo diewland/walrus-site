534e174909ad5f7e19a4fbd223764a35a75cef3f
diewland <diewland@gmail.com>
2025-05-23 22:34:20 +0700

    update publish script

--- publish.sh
+++ publish.sh
@@ -3,5 +3,5 @@
 #site-builder update --epochs max ./docs/setting 0xbbdbcf6b38f76e8dc2bf75b5b17fe8c25a5147fec1f6fe9943f0830af50f9e51
 
 # https://thaisui.wal.app
-#site-builder publish --site-name 'thaiSUI' --epochs max ./docs/thaiSUI
-site-builder update --epochs max ./docs/thaiSUI 0x8ccf5bf03ed04682159fe9a0dc079602d156dfb0b92be1d129f44dd7e42381b8
+#site-builder publish --site-name 'jigSUI' --epochs max ./docs/jigsui
+site-builder update --epochs max ./docs/jigsui 0xf3293bdf19ba5e6a1df40cd6d4958e3ac250ab50996c7ef6876e3e0ce47c3bed
